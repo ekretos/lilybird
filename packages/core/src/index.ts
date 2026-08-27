87023e24b3ed6e9f6b49c8fecafe4843d51da915
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 13:55:58 +0530

    export shard manager

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export type * from "./typings/index.js";
 
 export { REST } from "./http/rest.js";
 export * as CDN from "./http/cdn.js";
+export { ShardManager } from "./ws/manager.js";
 
 export * from "./enums/index.js";
 export * from "./client.js";
