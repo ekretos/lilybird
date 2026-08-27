e26fd2ae8f2104f03ea293b8b1b0a76d5803ab59
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 20:16:28 +0530

    refactor(core): expose decoupled compiler API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,8 +2,9 @@ export type * from "./typings/index.js";
 
 export { REST } from "./http/rest.js";
 export * as CDN from "./http/cdn.js";
-export { ShardManager } from "./ws/manager.js";
 
 export * from "./enums/index.js";
+export * from "./ws/manager.js";
+export * from "./compiler.js";
 export * from "./client.js";
 export * from "./utils.js";
